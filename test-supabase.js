afeecf3614c110cc219a97eaef418f8a22b1ebbb
Chaonick <hacquiernick@proton.me>
2025-08-29 21:24:08 +0200

    Add .env support and cleanup

--- test-supabase.js
+++ test-supabase.js
@@ -1,9 +1,9 @@
-// test-supabase.js
+import 'dotenv/config';
 import { createClient } from '@supabase/supabase-js';
 
 const supabase = createClient(
-  'https://hvgaakqmdqvezsfhxcdi.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh2Z2Fha3FtZHF2ZXpzZmh4Y2RpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY0ODY0NzQsImV4cCI6MjA3MjA2MjQ3NH0.zWSG74DiD5aDIb-TeRIP2ZfsQavkh3vszwJZb-1e3lE'
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_ANON_KEY
 );
 
 async function test() {
